478bc0b11822606c27d76c6ca6ae5fd31b3feb40
f1nwe <kaban.volodymyr@gmail.com>
2017-02-18 12:14:50 +0200

    HTTP, updating heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import {AppComponent} from "./app.component";
 import {HeroService} from "./hero.service";
 import {DashboardComponent} from "./dashboard.component";
 import {AppRoutingModule} from "./app-routing.module";
+import {HttpModule} from "@angular/http";
+import {InMemoryWebApiModule} from "angular-in-memory-web-api";
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
     ],
     declarations: [
         AppComponent,
